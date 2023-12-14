3a1b52daa40116c98c58c4c7a2d80f51f8632201
Moritz Waldleben <moritz@waldleben.ch>
2023-12-14 15:28:55 +0100

    remove flake sops-nix input

--- flake.nix
+++ flake.nix
@@ -21,11 +21,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
