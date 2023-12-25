c075017218e201cba2259d797e1c8d29f8dd2276
Moritz Waldleben <moritz@waldleben.ch>
2023-12-25 23:30:04 +0100

    add impermance module to home-manager

--- home/moritz/default.nix
+++ home/moritz/default.nix
@@ -1,6 +1,8 @@
 { inputs, outputs, config, ... }: {
-  imports = [ inputs.nix-colors.homeManagerModule ]
-    ++ (builtins.attrValues outputs.homeManagerModules);
+  imports = [ 
+    inputs.impermanence.nixosModules.home-manager.impermanence
+    inputs.nix-colors.homeManagerModule
+  ] ++ (builtins.attrValues outputs.homeManagerModules);
 
   programs = { home-manager.enable = true; };
 
@@ -10,13 +12,37 @@
     username = "moritz";
     homeDirectory = "/home/${config.home.username}";
     stateVersion = "23.11";
+    persistence = {
+      "/persist/home/${config.home.username}" = {
+        directories = [
+          "calendar"
+          "contacts"
+          "downloads"
+          "mail"
+          "nixos"
+          "notes"
+          "pictures"
+          "projs"
+          ".local"
+          ".config/nvim"
+          ".mozilla"
+          ".ssh"
+          ".gnupg"
+	        ".password-store"
+        ];
+        files = [
+          ".config/zsh/history"
+        ];
+        allowOther = true;
+      };
+    };
   };
 
   # session variables
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = 1;
     LIBSEAT_BACKEND = "logind";
-    # QT_QPA_PLATFORM = "wayland"; # no QT application yet
+    QT_QPA_PLATFORM = "wayland;xcb";
   };
 
   # restart systemd services when running "home-manager --flake . switch"
