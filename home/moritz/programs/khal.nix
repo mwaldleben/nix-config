8470b4a43e671dc58f03fedfc5095119a42c70a0
Moritz Waldleben <moritz@waldleben.ch>
2024-01-06 16:12:32 +0100

    khal: change date and time formats

--- home/moritz/programs/khal.nix
+++ home/moritz/programs/khal.nix
@@ -12,15 +12,16 @@
     [default]
     default_calendar = calendar
     highlight_event_days = true
+    timedelta = 31d
 
     [view]
     agenda_event_format = {calendar-color}{cancelled}{start-end-time-style} {title}{repeat-symbol}{reset}
 
     [locale]
     timeformat = %H:%M
-    dateformat = %d/%m/%Y
-    longdateformat = %d/%m/%Y
-    datetimeformat = %d/%m/%Y %H:%M
-    longdatetimeformat = %d/%m/%Y %H:%M
+    dateformat = %d.%m
+    longdateformat = %d.%m.%Y
+    datetimeformat = %d.%m.%Y %H:%M
+    longdatetimeformat = %d.%m.%Y %H:%M
   '';
 }
