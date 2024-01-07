20c5864f7c8cfd0e65bb1139db12a76585e5617e
Moritz Waldleben <moritz@waldleben.ch>
2024-01-07 12:54:11 +0100

    git: make merge the default strategy

--- home/moritz/programs/git.nix
+++ home/moritz/programs/git.nix
@@ -3,6 +3,9 @@
     enable = true;
     userEmail = "moritz@waldleben.ch";
     userName = "Moritz Waldleben";
-    extraConfig = { init = { defaultBranch = "main"; }; };
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      pull.rebase = false;
+    };
   };
 }
