19fefa5b6b9dbddad26ea98369c8ef5a0af2c5b6
Moritz Waldleben <moritz@waldleben.ch>
2024-02-15 13:36:38 +0100

    thinkpad: remove DHCP setting as this is handled by networkmanager

--- hosts/thinkpad/hardware-configuration.nix
+++ hosts/thinkpad/hardware-configuration.nix
@@ -58,16 +58,6 @@
 
   swapDevices = [{ device = "/swap/swapfile"; }];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wwp0s20u4.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = true;
 }
