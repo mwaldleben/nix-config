05facb86e9f9811143f63a0c03a990b114e45677
Moritz Waldleben <moritz@waldleben.ch>
2024-04-01 15:34:31 +0200

    waybar: add LC_TIME to start calendar of waybar at monday

--- hosts/common/locale.nix
+++ hosts/common/locale.nix
@@ -1,4 +1,7 @@
 {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = { LC_TIME = "de_CH.UTF-8"; };
+  };
   time.timeZone = "Europe/Zurich";
 }
