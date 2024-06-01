2be6208ce290e8cd22bb81616138a7c7687a354c
Moritz Waldleben <moritz@waldleben.ch>
2024-06-01 14:58:48 +0200

    hyprland: set default monitors for workspaces

--- home/moritz/hyprland/default.nix
+++ home/moritz/hyprland/default.nix
@@ -52,6 +52,8 @@
               }";
             position = "${toString m.x}x${toString m.y}";
           in "${m.name},${resolution},${position},1") (config.monitors);
+
+        workspace = map (m: "${m.name},${m.workspace}") (config.monitors);
       };
     };
   };
