96c766b9d125618e91913f13a1f0a4c084f6e090
Moritz Waldleben <moritz@waldleben.ch>
2024-06-05 12:11:23 +0200

    khal: add work and holidays calendar

--- home/moritz/accounts/calendar.nix
+++ home/moritz/accounts/calendar.nix
@@ -7,14 +7,64 @@ in {
         primary = true;
         vdirsyncer = {
           enable = true;
-          urlCommand =
-            [ "cat" "${config.sops.secrets.vdirsyncer-url-calendar.path}" ];
+          urlCommand = [
+            "cat"
+            "${config.sops.secrets.vdirsyncer-url-calendar-personal.path}"
+          ];
           userNameCommand =
             [ "cat" "${config.sops.secrets.vdirsyncer-userName.path}" ];
         };
         khal.enable = true;
         local = {
-          path = "~/.calendar";
+          path = "~/.calendar/personal";
+          type = "filesystem";
+          fileExt = ".ics";
+        };
+        remote = {
+          type = "caldav";
+          passwordCommand = [ "pass" "show" "caldav" ];
+        };
+      };
+      work = {
+        vdirsyncer = {
+          enable = true;
+          urlCommand = [
+            "cat"
+            "${config.sops.secrets.vdirsyncer-url-calendar-work.path}"
+          ];
+          userNameCommand =
+            [ "cat" "${config.sops.secrets.vdirsyncer-userName.path}" ];
+        };
+        khal = {
+          enable = true;
+          readOnly = true;
+        };
+        local = {
+          path = "~/.calendar/work";
+          type = "filesystem";
+          fileExt = ".ics";
+        };
+        remote = {
+          type = "caldav";
+          passwordCommand = [ "pass" "show" "caldav" ];
+        };
+      };
+      holidays = {
+        vdirsyncer = {
+          enable = true;
+          urlCommand = [
+            "cat"
+            "${config.sops.secrets.vdirsyncer-url-calendar-holidays.path}"
+          ];
+          userNameCommand =
+            [ "cat" "${config.sops.secrets.vdirsyncer-userName.path}" ];
+        };
+        khal = {
+          enable = true;
+          readOnly = true;
+        };
+        local = {
+          path = "~/.calendar/holidays";
           type = "filesystem";
           fileExt = ".ics";
         };
@@ -28,10 +78,16 @@ in {
 
   programs.vdirsyncer = { enable = true; };
 
-  sops.secrets.vdirsyncer-url-calendar = {
+  sops.secrets.vdirsyncer-userName = {
     sopsFile = ../../../hosts/thinkpad/secrets.yaml;
   };
-  sops.secrets.vdirsyncer-userName = {
+  sops.secrets.vdirsyncer-url-calendar-personal = {
+    sopsFile = ../../../hosts/thinkpad/secrets.yaml;
+  };
+  sops.secrets.vdirsyncer-url-calendar-work = {
+    sopsFile = ../../../hosts/thinkpad/secrets.yaml;
+  };
+  sops.secrets.vdirsyncer-url-calendar-holidays = {
     sopsFile = ../../../hosts/thinkpad/secrets.yaml;
   };
 }
--- home/moritz/programs/khal.nix
+++ home/moritz/programs/khal.nix
@@ -1,27 +1,35 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ khal ];
   xdg.configFile."khal/config".text = ''
-
     [calendars]
 
-    [[calendar]]
-    color = dark blue
-    path = ~/.calendar
-    type = calendar 
+      [[personal]]
+        path = ~/.calendar/personal/
+        color = dark blue
+
+      [[work]]
+        path = ~/.calendar/work/
+        color = yellow
+        readonly = True
+
+      [[holidays]]
+        path = ~/.calendar/holidays/
+        color = dark green 
+        readonly = True
 
     [default]
-    default_calendar = calendar
-    highlight_event_days = true
-    timedelta = 31d
+      default_calendar = personal
+      highlight_event_days = true
+      timedelta = 31d
 
     [view]
-    agenda_event_format = {calendar-color}{cancelled}{start-end-time-style} {title}{repeat-symbol}{reset}
+      agenda_event_format = {calendar-color}{cancelled}{start-end-time-style} {title}{repeat-symbol}{reset}
 
     [locale]
-    timeformat = %H:%M
-    dateformat = %d.%m
-    longdateformat = %d.%m.%Y
-    datetimeformat = %d.%m.%Y %H:%M
-    longdatetimeformat = %d.%m.%Y %H:%M
+      timeformat = %H:%M
+      dateformat = %d.%m
+      longdateformat = %d.%m.%Y
+      datetimeformat = %d.%m.%Y %H:%M
+      longdatetimeformat = %d.%m.%Y %H:%M
   '';
 }
