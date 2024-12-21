f13fef42cad54ad510d957199ee3d54e2a5ea26c
Moritz Waldleben <moritz@waldleben.ch>
2024-12-21 20:52:21 +0100

    sops: make ssh key path a string

--- home/moritz/cli/sops.nix
+++ home/moritz/cli/sops.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
   sops = {
-    age.sshKeyPaths = [ /persist/etc/ssh/ssh_host_ed25519_key ];
+    age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
     defaultSymlinkPath = "/run/user/1000/secrets";
     defaultSecretsMountPoint = "/run/user/1000/secrets.d";
   };
