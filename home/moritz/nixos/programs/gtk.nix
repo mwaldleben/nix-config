b608df981557ebbca886b7af1b85c2573432c667
Moritz Waldleben <moritz@waldleben.ch>
2025-02-08 10:20:37 +0100

    gtk: catppuccin cursor config changes (gtk catppuccin will soon be deprecated!)

--- home/moritz/nixos/programs/gtk.nix
+++ home/moritz/nixos/programs/gtk.nix
@@ -2,15 +2,22 @@
 {
   home.pointerCursor = {
     package = pkgs.catppuccin-cursors.frappeBlue;
-    name = "Catppucin-cursors-frappeBlue";
+    name = "catppuccin-frappe-blue-cursors";
     size = 32;
     gtk.enable = true;
   };
   gtk = {
     enable = true;
+    catppuccin.enable = true;
     font = {
       name = "${config.fontProfiles.regular.family} Sans";
       size = 11;
     };
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
+
+  home.packages = with pkgs; [
+    gnome-themes-extra
+  ];
 }
