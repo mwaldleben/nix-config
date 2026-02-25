b609cb0519c1a0f020dccb4e0238e0f58cf98de5
Moritz Waldleben <moritz@waldleben.ch>
2026-02-25 19:58:53 +0100

    remove licence of backup script

--- pkgs/backup/default.nix
+++ pkgs/backup/default.nix
@@ -12,9 +12,4 @@
     sudo rsync -razv --info=backup --delete --exclude={'.local','.mozilla'} /persist/home/moritz/ /run/media/moritz/MORITZ_SSD/backup/
   '';
 })
-// {
-  meta = with lib; {
-    license = licenses.mit;
-    platforms = platforms.all;
-  };
 }
