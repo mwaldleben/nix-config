a2fcbfd5243b62ed3c8fe1cb6f098c726f0da7d4
Moritz Waldleben <moritz@waldleben.ch>
2026-03-04 23:45:14 +0100

    remove docker

--- hosts/nixos/common/impermanence.nix
+++ hosts/nixos/common/impermanence.nix
@@ -7,7 +7,6 @@
       directories = [
         "/var/log"
         "/var/lib/bluetooth"
-        "/var/lib/docker"
         "/var/lib/nixos"
         "/var/lib/systemd/coredump"
         "/etc/NetworkManager/system-connections"
