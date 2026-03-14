1deee2053823550a34ee39ce9c685d0136a10fa6
Moritz Waldleben <moritz@waldleben.ch>
2026-03-14 13:08:14 +0100

    import nixos home-manager configs directly

--- home-manager/moritz-thinkpad.nix
+++ home-manager/moritz-thinkpad.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   imports = [
     ./common/catppuccin.nix
@@ -11,12 +11,65 @@
     ./common/todo-txt.nix
     ./common/ssh.nix
     ./common/zsh.nix
-    ./nixos
+    ./nixos/hyprland
+    ./nixos/ashell.nix
+    ./nixos/fuzzel.nix
+    ./nixos/gtk.nix
+    ./nixos/htop.nix
+    ./nixos/imv.nix
+    ./nixos/opencode.nix
+    ./nixos/pass.nix
+    ./nixos/picard.nix
+    ./nixos/qt.nix
+    ./nixos/sops.nix
+    ./nixos/xdg.nix
+    ./nixos/zathura.nix
+    ./nixos/gpg.nix
+    ./nixos/kanshi.nix
+    ./nixos/mako.nix
+    ./nixos/mpd.nix
+    ./nixos/syncthing.nix
+    ./nixos/udiskie.nix
   ];
 
   programs = {
     home-manager.enable = true;
   };
+
+  # packages without setup
+  home.packages = with pkgs; [
+    # gui
+    darktable
+    lollypop
+    libreoffice
+
+    # cli and utils
+    hyprshot
+    brightnessctl
+    cargo
+    exiftool
+    ghostscript
+    libnotify
+    android-file-transfer
+    mpv
+    nodejs
+    openjdk
+    pandoc
+    signal-desktop
+    urlscan
+    yt-dlp
+    wl-clipboard
+    # texlive.combined.scheme-full
+
+    # personal scripts (pkgs directory)
+    backup
+
+    # language servers
+    clang-tools
+    lua-language-server
+    stylua
+    tree-sitter
+  ];
 
   home = {
     username = "moritz";
