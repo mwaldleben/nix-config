0f7c538c7bbcfef7bccb1b9f462a02f9654ce704
Moritz Waldleben <moritz@waldleben.ch>
2026-03-26 09:58:18 +0100

    ghostty: update keybindings

--- home-manager/common/ghostty.nix
+++ home-manager/common/ghostty.nix
@@ -12,7 +12,13 @@ in
       confirm-close-surface = false;
       font-family = "monospace";
       font-size = if pkgs.stdenv.isDarwin then 15 else 13;
-      keybind = "clear";
+      keybind = [
+        "clear"
+        "ctrl+shift+c=copy_to_clipboard"
+        "super+c=copy_to_clipboard"
+        "ctrl+shift+v=paste_from_clipboard"
+        "super+v=paste_from_clipboard"
+      ];
       macos-titlebar-style = "hidden";
       maximize = true;
       resize-overlay = "never";
