b9889ee97ec367d0018e67755290470c75d07e6c
Moritz Waldleben <moritz@waldleben.ch>
2026-05-23 14:49:57 +0200

    ashell: update config

--- home-manager/nixos/ashell.nix
+++ home-manager/nixos/ashell.nix
@@ -6,7 +6,7 @@
     settings = {
       modules = {
         left = [ "Workspaces" ];
-        center = [ "Clock" ];
+        center = [ "Tempo" ];
         right = [
           "MediaPlayer"
           [
@@ -22,6 +22,23 @@
       appearance = {
         style = "Islands";
         scale_factor = 1;
+        # cattpuccin nix does not support ashell
+        background_color = {
+          base = "#303446";
+        };
+        primary_color = {
+          base = "#8caaee";
+        };
+        secondary_color = {
+          base = "#626880";
+        };
+        success_color = {
+          base = "#a6d189";
+        };
+        danger_color = {
+          base = "#e78284";
+        };
+        text_color = "#c6d0f5";
       };
       media_player = {
         max_title_length = 40;
